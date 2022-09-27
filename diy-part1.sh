6010a146a3ad5c17d572b3d25fc4ba27cf3566a5
zhangxxxl <113765533+zhangxxxl@users.noreply.github.com>
2022-09-27 16:04:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
